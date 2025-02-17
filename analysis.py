803a7336db08b69e194c43c3b63dd3e1322d8b84
Pranav Chaudhari <waytopranav22@gmail.com>
2025-02-17 14:33:05 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,8 +1,11 @@
-elif event_name in ["CreateSnapshot", "DeleteSnapshot", "ModifySnapshotAttribute"]:
-        request_parameters = event.get("detail", {}).get("requestParameters", {})
-        snapshot_id = request_parameters.get("snapshotId")
-
-        if snapshot_id:
-            print(f"EBS Snapshot Modified: {snapshot_id}, By User: {user_email}")
+# Handling EBS Drift Detection
+        elif event_source == "aws.ec2" and event["detail"]["eventName"] in [
+            "CreateSnapshot", 
+            "DeleteSnapshot", 
+            "ModifySnapshotAttribute" ]:
+            
+            request_parameters = event.get("detail", {}).get("requestParameters", {})
+            snapshot_id = request_parameters.get("snapshotId")
+            print(f"EBS Volume Modified: {snapshot_id}, By User: {user_email}")
             stack_name = find_stack(snapshot_id, "AWS::EC2::Snapshot")
             resource_name = snapshot_id
