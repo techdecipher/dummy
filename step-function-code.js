f139f4778444b6f2e594839cca707b20c24be22c
Pranav Chaudhari <waytopranav22@gmail.com>
2025-10-16 23:29:27 +0530

    Update step-function-code.js

--- step-function-code.js
+++ step-function-code.js
@@ -1,1 +1,42 @@
-An error occurred while executing the state 'Invoke_servicenow_lambda' (entered at the event id #12). The function 'States.JsonToString($.lambda3_input)' had the following error: The JsonPath argument for the field '$.lambda3_input' could not be found in the input '{"statusCode":200,"body":{"path":"scratch/terragrunt.hcl","commit_sha":"a0e40ada0ae284bc0cc428ef145e34a3e7ad5dff","commit_url":"https://github.com/Toyota-Motor-North-America/tbdpt-pnpaccess-aws-infra-live/commit/a0e40ada0ae284bc0cc428ef145e34a3e7ad5dff"}}'
+{
+  "StartAt": "Invoke_azure_Lambda",
+  "States": {
+    "Invoke_azure_Lambda": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::lambda:invoke",
+      "Parameters": {
+        "FunctionName": "arn:aws:lambda:us-east-1:5555555555:function:tbdp-test-lambda",
+        "Payload": { "action.$": "$.lambda1_input.action" }
+      },
+      "ResultPath": "$.azureResult",     // attach result here
+      "Next": "Invoke_git_Lambda"
+    },
+
+    "Invoke_git_Lambda": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::lambda:invoke",
+      "Parameters": {
+        "FunctionName": "arn:aws:lambda:us-east-1:5555555555:function:github_lambda",
+        "Payload": {}                     // no input
+      },
+      "ResultPath": "$.gitResult",        // attach here, keep root intact
+      "Next": "Invoke_servicenow_lambda"
+    },
+
+    "Invoke_servicenow_lambda": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::lambda:invoke",
+
+      "InputPath": "$.lambda3_input",     // show only lambda3_input to this state
+
+      "Parameters": {
+        "FunctionName": "arn:aws:lambda:us-east-1:5555555555:function:ServiceNow_lambda:$LATEST",
+        "Payload": {
+          "body.$": "States.JsonToString($)"   // matches your Lambda-3 test (event.body string)
+        }
+      },
+      "OutputPath": "$.Payload",
+      "End": true
+    }
+  }
+}
