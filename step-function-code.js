aa37935f8fae1354f975584655f5df23638c2e43
Pranav Chaudhari <waytopranav22@gmail.com>
2025-10-17 07:28:45 +0530

    Update step-function-code.js

--- step-function-code.js
+++ step-function-code.js
@@ -1,62 +1,9 @@
+Status: Succeeded
+Test Event Name: mytest
+
+Response:
 {
-  "lambda1_input": {
-    "action": "remove"
-  },
-  "lambda3_input": {
-    "tbdp_start": "Analysis",
-    "v_tbdp_date_accomplished": "2025-10-13 11:42:46 AM",
-    "v_tbdp_list_database": "NA",
-    "v_tbdp_environment": "Dev"
-  },
-  "azureResult": {
-    "ExecutedVersion": "$LATEST",
-    "Payload": {
-      "action": "remove",
-      "statusCode": 404,
-      "result": "{\"error\":{\"code\":\"Request_ResourceNotFound\",\"message\":\"Resource '35cce5c7-2e6b-4171-8fee-4911ae6e0d07' does not exist or one of its queried reference-property objects are not present.\",\"innerError\":{\"date\":\"2025-10-17T01:31:47\",\"request-id\":\"adc6eb81-edc4-4a6c-bc83-3c238ee4abf1\",\"client-request-id\":\"adc6eb81-edc4-4a6c-bc83-3c238ee4abf1\"}}}"
-    },
-    "SdkHttpMetadata": {
-      "AllHttpHeaders": {
-        "X-Amz-Executed-Version": [
-          "$LATEST"
-        ],
-        "x-amzn-Remapped-Content-Length": [
-          "0"
-        ],
-        "Connection": [
-          "keep-alive"
-        ],
-        "x-amzn-RequestId": [
-          "ac2b4519-c131-4974-924a-d12dfff109e9"
-        ],
-        "Content-Length": [
-          "418"
-        ],
-        "Date": [
-          "Fri, 17 Oct 2025 01:31:47 GMT"
-        ],
-        "X-Amzn-Trace-Id": [
-          "Root=1-68f19c81-68d85a65586fe32b59ce2308;Parent=60bedb66dff9b05a;Sampled=1;Lineage=1:d615684e:0"
-        ],
-        "Content-Type": [
-          "application/json"
-        ]
-      },
-      "HttpHeaders": {
-        "Connection": "keep-alive",
-        "Content-Length": "418",
-        "Content-Type": "application/json",
-        "Date": "Fri, 17 Oct 2025 01:31:47 GMT",
-        "X-Amz-Executed-Version": "$LATEST",
-        "x-amzn-Remapped-Content-Length": "0",
-        "x-amzn-RequestId": "ac2b4519-c131-4974-924a-d12dfff109e9",
-        "X-Amzn-Trace-Id": "Root=1-68f19c81-68d85a65586fe32b59ce2308;Parent=60bedb66dff9b05a;Sampled=1;Lineage=1:d615684e:0"
-      },
-      "HttpStatusCode": 200
-    },
-    "SdkResponseMetadata": {
-      "RequestId": "ac2b4519-c131-4974-924a-d12dfff109e9"
-    },
-    "StatusCode": 200
-  }
+  "action": "remove",
+  "statusCode": 204,
+  "result": ""
 }
